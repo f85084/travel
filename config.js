69e1c348ea274bf7fbe8b179473a574ea1111cb5
Anna <anna22.chen@ehsn.com.tw>
2026-01-29 11:55:14 +0800

    fix

---
+++ config.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyAFIJsMiLTcM1W7RvZ9xm9DJhBaT4aSyn4",
+  authDomain: "travel-6b530.firebaseapp.com",
+  projectId: "travel-6b530",
+  storageBucket: "travel-6b530.firebasestorage.app",
+  messagingSenderId: "631308723722",
+  appId: "1:631308723722:web:f6568da92693c14e3761f3",
+  measurementId: "G-K0FP6GBL0G",
+};
